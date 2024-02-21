b43802bed61ebe56db3e993e3a0e80c6d6788d70
mckayschaalje <mckayschaalje@gmail.com>
2024-02-20 21:27:34 -0700

    trying to get link to site to work

--- streamlit_template/main_page.py
+++ streamlit_template/main_page.py
@@ -5,5 +5,5 @@ st.sidebar.markdown("# Main Page")
 
 st.write("Click on a page to see racer or kart stats")
 
-link = '[To my Github Pages Site (https://mckay-schaalje-mariokart-stats.streamlit.app/racer_stats)'
+link = '[To my Github Pages Site (https://mckayschaalje.github.io/github_assignment)'
 st.markdown(link, unsafe_allow_html=True)
